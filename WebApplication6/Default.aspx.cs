77dda97387b641d1a69313c85f73877344297a28
Benjamin Stock <benjaminjoshuastock@gmail.com>
2017-02-23 21:13:55 +0000

    got database working, but no file uploads yet

--- WebApplication6/Default.aspx.cs
+++ WebApplication6/Default.aspx.cs
@@ -11,15 +11,16 @@ namespace WebApplication6
 {
     public partial class _Default : Page
     {
+        Int32 temp = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
             {
                 SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
                 conn.Open();
-                string checkuser = "select count(*) from [Table] where Username ='" + NameBox.Text + "' ";
+                string checkuser = "select count(*) from [Table] where Full_Name ='" + NameBox.Text + "' ";
                 SqlCommand com = new SqlCommand(checkuser, conn);
-                int temp = (Int32)com.ExecuteScalar();
+                temp = Convert.ToInt32(com.ExecuteScalar().ToString());
                 if (temp == 1)
                 {
                     Response.Write("Details Already Added.");
@@ -35,7 +36,7 @@ namespace WebApplication6
 
                 SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
                 conn.Open();
-                string insertQuery = "insert into Table (Full_Name, Email_Address, Institution, ZipFileLocation) values (@name, @email, @institution, @ziplocation)";
+                string insertQuery = "insert into [Table] (Full_Name, Email_Address, Institution, ZipFileLocation) values (@name, @email, @institution, @ziplocation)";
                 SqlCommand com = new SqlCommand(insertQuery, conn);
                 com.Parameters.AddWithValue("@name", NameBox.Text);
                 com.Parameters.AddWithValue("@email", EmaiBox.Text);
