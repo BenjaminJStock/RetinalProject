bfaf5ab24b491599f34cf41d7fd11e1bf77c0f20
Benjamin Stock <benjaminjoshuastock@gmail.com>
2017-02-27 12:58:49 +0000

    added sql for easy database making in the labs

---
+++ WebApplication6/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]              INT        DEFAULT (NEXT VALUE FOR [dbo].[Table_Id_Sequence]) NOT NULL,
+    [TimeNDate]       TEXT NULL,
+	[Full_Name]       NCHAR (50) NOT NULL,
+    [Email_Address]   NCHAR (50) NOT NULL,
+    [Institution]     NCHAR (50) NOT NULL,
+    [ZipFileLocation] TEXT       NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
